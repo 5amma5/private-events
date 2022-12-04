1273ac0ee1e86227415fffcecd658e95051d18c3
5amma5 <sam.anders5@gmail.com>
2022-12-04 05:59:34 -0700

    Added association between event creator(User) and event. Created user show page which shows all of a user's created events.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # get "users/:id" => 'users#show'
   root "events#index"
-  get 'events/index'
+  devise_for :users
+  resources :events, only: [:index]
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_144805) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_03_012238) do
   create_table "events", force: :cascade do |t|
     t.string "location"
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_144805) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
